cef0a737f0fcc36315b83b667ffe248e284b7289
aditik-20 <ak.krishnaaditi@gmail.com>
2026-04-01 09:04:14 +0530

    uc2 - Add Passenger

---
+++ TCMA/src/TrainManageApp.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class TrainManageApp {
+    public static void main(String[] args) {
+
+        ArrayList<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger Bogies after addition:");
+        System.out.println(passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nPassenger Bogies after removal of AC Chair:");
+        System.out.println(passengerBogies);
+
+        if (passengerBogies.contains("Sleeper")) {
+            System.out.println("\nSleeper bogie exists in the train.");
+        } else {
+            System.out.println("\nSleeper bogie does not exist in the train.");
+        }
+
+        System.out.println("\nFinal Passenger Bogies List:");
+        System.out.println(passengerBogies);
+    }
+}
