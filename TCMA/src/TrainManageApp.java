b5257f8406cd245f2a8e2675538dc711716814b6
aditik-20 <ak.krishnaaditi@gmail.com>
2026-04-16 22:23:36 +0530

    Uc9

--- TCMA/src/TrainManageApp.java
+++ TCMA/src/TrainManageApp.java
@@ -1,46 +1,62 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
-class Bogie {
-    private String name;
-    private int capacity;
+abstract class Bogie {
+    protected String bogieId;
+    protected String type;
+    protected int capacity;
 
-    public Bogie(String name, int capacity) {
-        this.name = name;
+    public Bogie(String bogieId, String type, int capacity) {
+        this.bogieId = bogieId;
+        this.type = type;
         this.capacity = capacity;
     }
 
-    public String getName() {
-        return name;
+    public String getBogieId() {
+        return bogieId;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public int getCapacity() {
         return capacity;
     }
 
-    @Override
     public String toString() {
-        return name + " (Capacity: " + capacity + ")";
+        return "Bogie ID=" + bogieId + ", Type=" + type + ", Capacity=" + capacity;
+    }
+}
+
+class PassengerBogie extends Bogie {
+    public PassengerBogie(String bogieId, String type, int capacity) {
+        super(bogieId, type, capacity);
     }
 }
 
 public class TrainManageApp {
+
+    public static Map<String, List<Bogie>> groupBogiesByType(List<Bogie> bogies) {
+        return bogies.stream()
+                .collect(Collectors.groupingBy(Bogie::getType));
+    }
+
     public static void main(String[] args) {
-        // Step 1: Create a list of passenger bogies
         List<Bogie> bogies = new ArrayList<>();
-        bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("AC Chair", 56));
-        bogies.add(new Bogie("First Class", 24));
 
-        System.out.println("Before Sorting:");
-        for (Bogie b : bogies) {
-            System.out.println(b);
-        }
+        bogies.add(new PassengerBogie("PB101", "Sleeper", 72));
+        bogies.add(new PassengerBogie("PB102", "AC Chair", 60));
+        bogies.add(new PassengerBogie("PB103", "First Class", 45));
+        bogies.add(new PassengerBogie("PB104", "Sleeper", 80));
+        bogies.add(new PassengerBogie("PB105", "AC Chair", 70));
 
-        bogies.sort(Comparator.comparingInt(Bogie::getCapacity));
+        Map<String, List<Bogie>> grouped = groupBogiesByType(bogies);
 
-        System.out.println("\nAfter Sorting by Capacity:");
-        for (Bogie b : bogies) {
-            System.out.println(b);
+        for (Map.Entry<String, List<Bogie>> entry : grouped.entrySet()) {
+            System.out.println(entry.getKey());
+            entry.getValue().forEach(System.out::println);
+            System.out.println();
         }
     }
 }
